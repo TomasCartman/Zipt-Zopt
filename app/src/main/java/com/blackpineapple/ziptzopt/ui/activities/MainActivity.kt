6425fe6f6419a939c53cf32ff243234dadf8febe
Tomas Cartman <marco12y@outlook.com>
2021-04-01 22:58:04 -0300

    add few layouts

---
+++ app/src/main/java/com/blackpineapple/ziptzopt/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.blackpineapple.ziptzopt.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.blackpineapple.ziptzopt.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
