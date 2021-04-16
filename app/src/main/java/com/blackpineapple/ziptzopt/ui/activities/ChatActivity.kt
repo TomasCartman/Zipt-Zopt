81c1a9d2755b51b3c5e90602e068e23acc1098b0
Tomas Cartman <marco12y@outlook.com>
2021-04-16 16:40:42 -0300

    add message bubbles layouts

--- app/src/main/java/com/blackpineapple/ziptzopt/ui/activities/ChatActivity.kt
+++ app/src/main/java/com/blackpineapple/ziptzopt/ui/activities/ChatActivity.kt
@@ -1,19 +1,35 @@
 package com.blackpineapple.ziptzopt.ui.activities
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.ViewModelProvider
 import com.blackpineapple.ziptzopt.R
+import com.blackpineapple.ziptzopt.viewmodel.ChatActivityViewModel
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var toolbar: MaterialToolbar
+    private lateinit var floatingActionButton: FloatingActionButton
+    private lateinit var sendMessageEditText: TextInputEditText
+    private lateinit var chatActivityViewModel: ChatActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
+        chatActivityViewModel = ViewModelProvider
+            .NewInstanceFactory().create(ChatActivityViewModel::class.java)
+
         toolbar = findViewById(R.id.configurations_toolbar)
         setupToolbar()
+
+        floatingActionButton = findViewById(R.id.floating_action_button)
+        sendMessageEditText = findViewById(R.id.send_message_editText)
+        setupSendMessageEditText()
     }
 
     private fun setupToolbar() {
@@ -21,6 +37,16 @@ class ChatActivity : AppCompatActivity() {
             onBackPressed()
         }
         toolbar.title = intent.getStringExtra(ARG_NAME)
+    }
+
+    private fun setupSendMessageEditText() {
+        sendMessageEditText.doOnTextChanged { text, start, before, count ->
+            if(text != null && text.isNotEmpty()) {
+                floatingActionButton.setImageResource(R.drawable.icon_send)
+            } else {
+                floatingActionButton.setImageResource(R.drawable.icon_mic)
+            }
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/blackpineapple/ziptzopt/viewmodel/ChatActivityViewModel.kt
@@ -0,0 +1,6 @@
+package com.blackpineapple.ziptzopt.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ChatActivityViewModel : ViewModel() {
+}
